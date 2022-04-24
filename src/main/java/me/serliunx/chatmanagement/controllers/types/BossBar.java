3361644f257ff2684d14ff0c2c8ff0eb1983206e
SerLiunx-ctrl <17689543@qq.com>
2022-04-24 09:20:54 +0800

    重构: 为BossBar相关功能写了个工具类, 方便调用.

--- src/main/java/me/serliunx/chatmanagement/controllers/types/BossBar.java
+++ src/main/java/me/serliunx/chatmanagement/controllers/types/BossBar.java
@@ -6,55 +6,31 @@ import me.serliunx.chatmanagement.database.entities.Format;
 import me.serliunx.chatmanagement.database.entities.User;
 import me.serliunx.chatmanagement.enums.YamlFile;
 import me.serliunx.chatmanagement.placeholders.Placeholders;
+import me.serliunx.chatmanagement.utils.BossBarUtils;
 import me.serliunx.chatmanagement.utils.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
-import java.util.ArrayList;
-import java.util.List;
 
 public final class BossBar implements Controller {
 
-    private int maxTicks;
-    private String barColor,barStyle,prefix,suffix,message;
-
     @Override
     public void show(String text, User user, Format format) {
-
-        List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
-        maxTicks = ChatManagement.getInstance().getConfigManager().getByConfigName(YamlFile.YAML_MAIN.getValue())
+        int maxTicks = ChatManagement.getInstance().getConfigManager().getByConfigName(YamlFile.YAML_MAIN.getValue())
                 .getConfiguration().getInt("bossBar.timer",80);
-        barColor = ChatManagement.getInstance().getConfigManager().getByConfigName(YamlFile.YAML_MAIN.getValue())
+        String barColor = ChatManagement.getInstance().getConfigManager().getByConfigName(YamlFile.YAML_MAIN.getValue())
                 .getConfiguration().getString("bossBar.color", "GREEN");
-        barStyle = ChatManagement.getInstance().getConfigManager().getByConfigName(YamlFile.YAML_MAIN.getValue())
+        String barStyle = ChatManagement.getInstance().getConfigManager().getByConfigName(YamlFile.YAML_MAIN.getValue())
                 .getConfiguration().getString("bossBar.style", "SOLID");
-        prefix = Placeholders.replacePlaceHolders(StringUtils.Color(format.getPrefix()), user.getPlayer());
-        suffix = Placeholders.replacePlaceHolders(StringUtils.Color(format.getSuffix()), user.getPlayer());
-        message = StringUtils.ColorWithPlayer(user.getPlayer(),text);
+        String prefix = Placeholders.replacePlaceHolders(StringUtils.Color(format.getPrefix()), user.getPlayer());
+        String suffix = Placeholders.replacePlaceHolders(StringUtils.Color(format.getSuffix()), user.getPlayer());
+        String message = StringUtils.ColorWithPlayer(user.getPlayer(),text);
 
-        new BukkitRunnable(){
-            int ticks = 0;
-            boolean done = false;
-
-            final org.bukkit.boss.BossBar bossBar = Bukkit.createBossBar(prefix + message + suffix,
-                    BarColor.valueOf(barColor), BarStyle.valueOf(barStyle));
-
-            @Override
-            public void run(){
-                if(done){
-                    bossBar.setVisible(false);
-                    return;
-                }
-                players.forEach(bossBar::addPlayer);
-                bossBar.setProgress(1 - (double) ticks / maxTicks);
-                bossBar.setVisible(true);
-                ticks++;
-                if(ticks >= maxTicks)
-                    done = true;
-            }
-        }.runTaskTimer(ChatManagement.getInstance(),0,1);
+        for (Player p:Bukkit.getOnlinePlayers()){
+            BossBarUtils.showText(p,prefix + message + suffix, BarColor.valueOf(barColor), BarStyle.valueOf(barStyle),
+                    1, maxTicks);
+        }
     }
 
     @Override
