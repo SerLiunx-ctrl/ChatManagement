13f3ec6f438686f70d1a0712f34ab4923cd0ed86
SerLiunx-ctrl <17689543@qq.com>
2022-04-30 12:49:08 +0800

    工具类: 新增一次性替换一个列表的占位符

--- src/main/java/me/serliunx/chatmanagement/placeholder/Placeholders.java
+++ src/main/java/me/serliunx/chatmanagement/placeholder/Placeholders.java
@@ -5,14 +5,36 @@ import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Placeholders {
 
+    /**
+     * 替换文本中出现的占位符
+     * @param rawText 文本
+     * @param player 玩家
+     * @return 替换过的文本
+     */
     public static String replacePlaceHolders(String rawText, Player player){
         if(ChatManagement.getInstance().isUsePapi())
             return PlaceholderAPI.setPlaceholders(player, rawText);
         else
             return rawText;
+    }
+
+    /**
+     * 替换一个列表文本中出现的占位符
+     * @param rawTexts 文本列表
+     * @param player 玩家
+     * @return 替换过的文本列表
+     */
+    public static List<String> replaceAll(List<String> rawTexts, Player player){
+        List<String> replaced = new ArrayList<>();
+        for (String rawText : rawTexts) {
+            replaced.add(replacePlaceHolders(rawText, player));
+        }
+        return replaced;
     }
 
     @Nullable
