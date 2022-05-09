5a040bfbb69dc42136a009dfd21cf162781482eb
SerLiunx-ctrl <17689543@qq.com>
2022-05-09 09:48:43 +0800

    新版本: 1.0.2-dev

---
+++ src/main/java/me/serliunx/chatmanagement/util/Updater.java
@@ -0,0 +1,15 @@
+package me.serliunx.chatmanagement.util;
+
+import me.serliunx.chatmanagement.ChatManagement;
+
+public class Updater {
+
+    public final ChatManagement plugin;
+
+    public Updater() {
+        this.plugin = ChatManagement.getInstance();
+    }
+
+
+
+}
