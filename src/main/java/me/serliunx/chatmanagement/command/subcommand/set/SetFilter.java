c8660c90fe23154b51ccd538be759eef7b44f078
SerLiunx <17689543@qq.com>
2022-05-24 17:36:08 +0800

    指令: set指令待完善.

--- src/main/java/me/serliunx/chatmanagement/command/subcommand/set/SetFilter.java
+++ src/main/java/me/serliunx/chatmanagement/command/subcommand/set/SetFilter.java
@@ -1,25 +1,42 @@
 package me.serliunx.chatmanagement.command.subcommand.set;
 
+import me.serliunx.chatmanagement.ChatManagement;
 import me.serliunx.chatmanagement.command.Command;
+import me.serliunx.chatmanagement.database.entity.Filter;
 import org.bukkit.command.CommandSender;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class SetFilter extends Command {
+    private final ChatManagement instance;
 
     public SetFilter() {
         super(Collections.singletonList("filter"), "filter settings", "/chatm set filter", "",
                 false, Duration.ZERO);
+        instance = ChatManagement.getInstance();
     }
 
     @Override
     public boolean execute(CommandSender sender, String[] arguments) {
-        return false;
+        if(!getFilterNames().contains(arguments[2])){
+            sender.sendMessage("cannot find the filter: " + arguments[2]);
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender commandSender, org.bukkit.command.Command command, String label, String[] args) {
-        return null;
+        return getFilterNames();
+    }
+
+    private List<String> getFilterNames(){
+        List<String> filterNames = new ArrayList<>();
+        for(Filter filter:instance.getFilterManager().getFilters())
+            filterNames.add(filter.getName());
+        return filterNames;
     }
 }
